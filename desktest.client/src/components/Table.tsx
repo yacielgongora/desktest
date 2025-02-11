5d745fa76064ae640c8b90f8de3d9d0ecaf0508d
Yaciel Gongora <88797232+yacielgongora@users.noreply.github.com>
2025-02-11 09:20:19 -0500

    Update somes imports on Table component

--- desktest.client/src/components/Table.tsx
+++ desktest.client/src/components/Table.tsx
@@ -1,4 +1,5 @@
-﻿import React, { useState } from "react";
+import * as React from 'react';
+import { useState } from "react";
 
 import { Statement, TableWithPaginationProps } from "../types/Interfaces";
 
